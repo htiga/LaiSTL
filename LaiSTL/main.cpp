07de261629c6c2743eecb9cac77f060b53cd1649
htiga <htiga@hotmail.com>
2015-09-17 23:55:27 +0800

    ###

--- LaiSTL/main.cpp
+++ LaiSTL/main.cpp
@@ -1,4 +1,13 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include "Deque.h"
+#include <iostream>
 int main()
 {
+    {
+    }
+    _CrtDumpMemoryLeaks();
     return 0;
 }

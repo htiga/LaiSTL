4d99b09d14af370e76d1bd63689d4178baf2c200
htiga <htiga@hotmail.com>
2015-09-17 23:56:00 +0800

    add common test utilities

--- UnitTest/stdafx.h
+++ UnitTest/stdafx.h
@@ -8,31 +8,5 @@
 
 // Headers for CppUnitTest
 #include "CppUnitTest.h"
-
-
-// TODO: reference additional headers your program requires here
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-
-#define IS_TRUE Assert::IsTrue
-#define IS_FALSE Assert::IsFalse
-
-template<typename Container1, typename Container2>
-void AssertContainerEqual(const Container1 & tested, const Container2 & expected)
-{
-    IS_TRUE(tested.size() == expected.size());
-    for (int i = 0; i != tested.size(); ++i)
-    {
-        IS_TRUE(tested[i] == expected[i]);
-    }
-}
-
-
-template<typename Container, typename Object>
-void AssertContainerFilled(const Container & c, const Object & filledVal)
-{
-    for (int i = 0; i != c.size(); ++i)
-    {
-        IS_TRUE(c[i] == filledVal);
-    }
-}
+#include "CommonTestUtilities.h"
+#include <string>

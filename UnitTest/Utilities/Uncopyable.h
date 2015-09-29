a7c9672a65337c373bbebaafe7fb305067ed3e5b
htiga <htiga@hotmail.com>
2015-09-29 15:22:58 +0800

    remove namespace UnitTest

--- UnitTest/Utilities/Uncopyable.h
+++ UnitTest/Utilities/Uncopyable.h
@@ -1,52 +1,49 @@
 #pragma once
 
-namespace UnitTest
+class Uncopyable
 {
-    class Uncopyable
+public:
+    explicit Uncopyable(int id = 0) :
+        myId(id) { }
+
+    Uncopyable(Uncopyable && rhs) = default;
+
+    Uncopyable & operator=(Uncopyable && rhs) = default;
+
+    Uncopyable(const Uncopyable &) = delete;
+
+    Uncopyable & operator=(const Uncopyable &) = delete;
+
+    bool operator==(const Uncopyable & rhs) const noexcept
     {
-    public:
-        explicit Uncopyable(int id = 0) :
-            myId(id) { }
+        return myId == rhs.myId;
+    }
 
-        Uncopyable(Uncopyable && rhs) = default;
+    bool operator!=(const Uncopyable & rhs) const noexcept
+    {
+        return !(*this == rhs);
+    }
 
-        Uncopyable & operator=(Uncopyable && rhs) = default;
+    bool operator<(const Uncopyable & rhs) const noexcept
+    {
+        return myId < rhs.myId;
+    }
 
-        Uncopyable(const Uncopyable &) = delete;
+    bool operator>=(const Uncopyable & rhs) const noexcept
+    {
+        return !(*this < rhs);
+    }
 
-        Uncopyable & operator=(const Uncopyable &) = delete;
+    bool operator>(const Uncopyable & rhs) const noexcept
+    {
+        return !(rhs >= *this);
+    }
 
-        bool operator==(const Uncopyable & rhs) const noexcept
-        {
-            return myId == rhs.myId;
-        }
+    bool operator<=(const Uncopyable & rhs) const noexcept
+    {
+        return !(*this > rhs);
+    }
 
-        bool operator!=(const Uncopyable & rhs) const noexcept
-        {
-            return !(*this == rhs);
-        }
-
-        bool operator<(const Uncopyable & rhs) const noexcept
-        {
-            return myId < rhs.myId;
-        }
-
-        bool operator>=(const Uncopyable & rhs) const noexcept
-        {
-            return !(*this < rhs);
-        }
-
-        bool operator>(const Uncopyable & rhs) const noexcept
-        {
-            return !(rhs >= *this);
-        }
-
-        bool operator<=(const Uncopyable & rhs) const noexcept
-        {
-            return !(*this > rhs);
-        }
-
-    private:
-        int myId = 0;
-    };
-}
+private:
+    int myId = 0;
+};

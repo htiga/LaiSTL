ee730fb7255fdd2f22de1994840ef2559137fc12
htiga <htiga@hotmail.com>
2015-09-29 19:58:55 +0800

    add test cases for sequence constructor

--- UnitTest/Utilities/TestSequenceContainer.h
+++ UnitTest/Utilities/TestSequenceContainer.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "stdafx.h"
-#include "../Utilities/Uncopyable.h"
-#include <string>
 // TSC means "Test sequence container"
 
 template<typename Container>
@@ -17,9 +15,9 @@ void TSC_DefaultConstructorAux()
 
 #define TSC_DefaultConstructor(ContainerTemplate) \
 do { \
-    TSC_DefaultConstructorAux<ContainerTemplate<int>>(); \
-    TSC_DefaultConstructorAux<ContainerTemplate<std::string>>(); \
-    TSC_DefaultConstructorAux<ContainerTemplate<Uncopyable>>(); \
+    TSC_DefaultConstructorAux< ContainerTemplate<int> >(); \
+    TSC_DefaultConstructorAux< ContainerTemplate<std::string> >(); \
+    TSC_DefaultConstructorAux< ContainerTemplate<Uncopyable> >(); \
 } while (false)
 
 
@@ -47,10 +45,128 @@ void TSC_CountConstructorAux(const ValueType & val)
 
 #define TSC_CountConstructor(ContainerTemplate) \
 do { \
-    TSC_CountConstructorAux<ContainerTemplate<int>, STD_IVEC>(); \
-    TSC_CountConstructorAux<ContainerTemplate<std::string>, STD_SVEC>(); \
-    TSC_CountConstructorAux<ContainerTemplate<Uncopyable>, STD_UVEC>(); \
+    TSC_CountConstructorAux< ContainerTemplate<int>, STD_IVEC>(); \
+    TSC_CountConstructorAux< ContainerTemplate<std::string>, STD_SVEC>(); \
+    TSC_CountConstructorAux< ContainerTemplate<Uncopyable>, STD_UVEC>(); \
 \
-    TSC_CountConstructorAux<ContainerTemplate<int>, STD_IVEC>(42); \
-    TSC_CountConstructorAux<ContainerTemplate<std::string>, STD_SVEC>("hello"); \
+    TSC_CountConstructorAux< ContainerTemplate<int>, STD_IVEC>(42); \
+    TSC_CountConstructorAux< ContainerTemplate<std::string>, STD_SVEC>("hello"); \
+} while (false)
+
+
+template<typename Container, typename DataSet>
+void TSC_RangeConstructorAux(const DataSet & dataSet)
+{
+    for (auto & data : dataSet)
+    {
+        Container c(data.begin(), data.end());
+        AssertContainerEqual(c, data);
+    }
+}
+
+
+#define TSC_RangeConstructor(ContainerTemplate)\
+do { \
+    std::initializer_list<I_IL> intData =  \
+    {                                      \
+        {}, { 1 }, { 0,1,2,3,4,5,6,7,8,9 } \
+    };                                     \
+    TSC_RangeConstructorAux< ContainerTemplate<int> >(intData); \
+\
+    std::initializer_list<S_IL> strData =                   \
+    {                                                       \
+        {}, {"laistl"}, {"a", "b", "c", "dd", "eee", "fff"} \
+    };                                                      \
+    TSC_RangeConstructorAux< ContainerTemplate<std::string> >(strData); \
+} while (false)
+
+
+template<typename Container, typename DataSet>
+void TSC_BraceListConstructorAux(const DataSet & dataset)
+{
+    for (auto & data : dataset)
+    {
+        Container c = { data };
+        AssertContainerEqual(c, data);
+    }
+}
+
+
+#define TSC_BraceListConstructor(ContainerTemplate) \
+do { \
+    std::initializer_list<I_IL> intData = \
+    {                                     \
+        {}, {1}, {1, 2}, {1,2,3,4,5,6,7,8,9} \
+    };                                      \
+    TSC_BraceListConstructorAux< ContainerTemplate<int> >(intData); \
+\
+    std::initializer_list<S_IL> strData =                   \
+    {                                                       \
+        {}, {"laistl"}, {"lai", "stl"}, {"a", "b", "c", "dd", "eee", "fff"} \
+    };                                                      \
+    TSC_BraceListConstructorAux< ContainerTemplate<std::string> >(strData); \
+} while (false)
+
+
+template<typename Container, typename DataSet>
+void TSC_CopyConstructorAux(const DataSet & dataset)
+{
+    for (auto & data : dataset)
+    {
+        Container c(data);
+        Container copy(c);
+        AssertContainerEqual(c, data);
+        AssertContainerEqual(c, copy);
+        c.~Container();
+        AssertContainerEqual(copy, data);
+    }
+}
+
+
+#define TSC_CopyConstructor(ContainerTemplate) \
+do { \
+    std::initializer_list<I_IL> intData = \
+    {                                     \
+        {}, {1}, {1, 2}, {1,2,3,4,5,6,7,8,9} \
+    };                                      \
+    TSC_CopyConstructorAux< ContainerTemplate<int> >(intData); \
+\
+    std::initializer_list<S_IL> strData =                   \
+    {                                                       \
+        {}, {"laistl"}, {"lai", "stl"}, {"a", "b", "c", "dd", "eee", "fff"} \
+    };                                                      \
+    TSC_CopyConstructorAux< ContainerTemplate<std::string> >(strData); \
+} while (false)
+
+
+template<typename Container, typename DataSet>
+void TSC_MoveConstructorAux(const DataSet & dataset)
+{
+    for (auto & data : dataset)
+    {
+        Container c(data);
+        Container copy(std::move(c));
+        c.~Container();
+        AssertContainerEqual(copy, data);
+    }
+}
+
+
+#define TSC_MoveConstructor(ContainerTemplate) \
+do { \
+    std::initializer_list<I_IL> intData = \
+    {                                     \
+        {}, {1}, {1, 2}, {1,2,3,4,5,6,7,8,9} \
+    };                                      \
+    TSC_MoveConstructorAux< ContainerTemplate<int> >(intData); \
+\
+    std::initializer_list<S_IL> strData =                   \
+    {                                                       \
+        {}, {"laistl"}, {"lai", "stl"}, {"a", "b", "c", "dd", "eee", "fff"} \
+    };                                                      \
+    TSC_MoveConstructorAux< ContainerTemplate<std::string> >(strData); \
+\
+    ContainerTemplate<Uncopyable> c(10); \
+    ContainerTemplate<Uncopyable> moved(std::move(c)); \
+    AssertContainerEqual(moved, STD_UVEC(10)); \
 } while (false)

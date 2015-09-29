2829e06c944dbea3f6cd941d02276e00dd4a6383
htiga <htiga@hotmail.com>
2015-09-29 15:01:49 +0800

    add class Uncopyable

---
+++ UnitTest/Utilities/Uncopyable.h
@@ -0,0 +1,52 @@
+#pragma once
+
+namespace UnitTest
+{
+    class Uncopyable
+    {
+    public:
+        explicit Uncopyable(int id = 0) :
+            myId(id) { }
+
+        Uncopyable(Uncopyable && rhs) = default;
+
+        Uncopyable & operator=(Uncopyable && rhs) = default;
+
+        Uncopyable(const Uncopyable &) = delete;
+
+        Uncopyable & operator=(const Uncopyable &) = delete;
+
+        bool operator==(const Uncopyable & rhs) const noexcept
+        {
+            return myId == rhs.myId;
+        }
+
+        bool operator!=(const Uncopyable & rhs) const noexcept
+        {
+            return !(*this == rhs);
+        }
+
+        bool operator<(const Uncopyable & rhs) const noexcept
+        {
+            return myId < rhs.myId;
+        }
+
+        bool operator>=(const Uncopyable & rhs) const noexcept
+        {
+            return !(*this < rhs);
+        }
+
+        bool operator>(const Uncopyable & rhs) const noexcept
+        {
+            return !(rhs >= *this);
+        }
+
+        bool operator<=(const Uncopyable & rhs) const noexcept
+        {
+            return !(*this > rhs);
+        }
+
+    private:
+        int myId = 0;
+    };
+}

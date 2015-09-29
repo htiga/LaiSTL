12545b8afb44645dfab03d572016a5a6cc9449cf
htiga <htiga@hotmail.com>
2015-09-29 19:58:37 +0800

    add macros

--- UnitTest/Utilities/macros.h
+++ UnitTest/Utilities/macros.h
@@ -13,17 +13,19 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 #define STD_IVEC std::vector<int>
 #define STD_SVEC std::vector<std::string>
 #define STD_UVEC std::vector<Uncopyable>
+
 #define I_IL std::initializer_list<int>
 #define S_IL std::initializer_list<std::string>
+#define U_IL std::initializer_list<Uncopyable>
 
 
 template<typename Container1, typename Container2>
 void AssertContainerEqual(const Container1 & tested, const Container2 & expected)
 {
     IS_TRUE(tested.size() == expected.size());
-    auto iter1 = tested.cbegin();
-    auto iter2 = expected.cbegin();
-    while (iter1 != tested.cend())
+    auto iter1 = tested.begin();
+    auto iter2 = expected.begin();
+    while (iter1 != tested.end())
     {
         IS_TRUE(*iter1++ == *iter2++);
     }

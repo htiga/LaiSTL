5fac9feff0bbd9814ec966a074091fde0e1b4a0b
htiga <htiga@hotmail.com>
2015-10-01 00:37:24 +0800

    move away a function

--- UnitTest/Utilities/macros.h
+++ UnitTest/Utilities/macros.h
@@ -17,16 +17,3 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 #define I_IL std::initializer_list<int>
 #define S_IL std::initializer_list<std::string>
 #define U_IL std::initializer_list<Uncopyable>
-
-
-template<typename Container1, typename Container2>
-void AssertContainerEqual(const Container1 & tested, const Container2 & expected)
-{
-    IS_TRUE(tested.size() == expected.size());
-    auto iter1 = tested.begin();
-    auto iter2 = expected.begin();
-    while (iter1 != tested.end())
-    {
-        IS_TRUE(*iter1++ == *iter2++);
-    }
-}

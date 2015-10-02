527bc654fb172f17f4c86ce1edb0b0b105abd1f1
htiga <htiga@hotmail.com>
2015-10-02 12:55:22 +0800

    add function AssertContainerFillWith

--- UnitTest/Utilities/TestContainerUtilities.h
+++ UnitTest/Utilities/TestContainerUtilities.h
@@ -12,3 +12,14 @@ void AssertContainerEqual(const Container1 & tested, const Container2 & expected
         IS_TRUE(*iter1++ == *iter2++);
     }
 }
+
+
+template<typename Container, typename ValueType>
+void AssertContainerFillWith(const Container & c, const ValueType & val)
+{
+    auto iter = c.begin();
+    while (iter != c.end())
+    {
+        IS_TRUE(*iter++ == val);
+    }
+}

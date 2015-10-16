a79930722f0ce8af469eee7041a8c3b2efd005c7
htiga <htiga@hotmail.com>
2015-10-16 20:46:56 +0800

    Add type alias

--- LaiSTL/Forward_List.h
+++ LaiSTL/Forward_List.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <memory>
+#include <iterator>
+
+template<typename T, typename Allocator = std::allocator<T>>
+class forward_list
+{
+public:
+    using value_type      = T;
+    using allocator_type  = Allocator;
+    using size_type       = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference       = value_type &;
+    using const_reference = const value_type &;
+    using pointer         = std::allocator_traits<Allocator>::pointer;
+    using const_pointer   = std::allocator_traits<Allocator>::const_pointer;
+};

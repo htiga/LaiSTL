f24ad0e4ffa7d563aba2f5d986646f5d450a5513
htiga <htiga@hotmail.com>
2015-11-14 20:42:49 +0800

    ...

--- LaiSTL/Set.h
+++ LaiSTL/Set.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "./Details/CommonTypes.h"
 #include "./Details/RB_Tree.h"
 #include <memory>
 #include <iterator>
@@ -9,18 +10,17 @@ namespace lai
     template<typename TKey, typename TCompare, typename Allocator>
     struct Set_TreeTraits
     {
-        using key_type = TKey;
-        using value_type = TKey;
-        using key_compare = TCompare;
-        using value_compare = TCompare;
+        using key_type       = TKey;
+        using value_type     = TKey;
+        using key_compare    = TCompare;
+        using value_compare  = TCompare;
         using allocator_type = Allocator;
+        using IsMulti        = details::FalseType;
 
         static const key_type & getKey(const value_type & value) noexcept
         {
             return value;
         }
-
-        constexpr static bool isMulti = false;
     };
 
     template<typename TKey,
@@ -32,15 +32,78 @@ namespace lai
         using MyBase = details::Tree<Set_TreeTraits<TKey, Compare, Allocator>>;
 
     public:
-        explicit set(const key_compare & comp = key_compare()) :
-            MyBase(comp) { }
+        using MyBase::Tree;
 
-        template<typename InputIt,
-            typename = std::enable_if_t<!std::is_integral<InputIt>::value >>
-            set(InputIt first, InputIt last, const key_compare & comp = key_compare()) :
-            MyBase(first, last, comp) { }
+        set() :
+            set(key_compare()) { }
 
-        set(std::initializer_list<value_type> iList, const key_compare & comp = key_compare()) :
-            MyBase(iList, comp) { }
+        set(const set & rhs) :
+            MyBase(rhs) { }
+
+        set(set && rhs) :
+            MyBase(std::move(rhs)) { }
+
+        set & operator=(const set & rhs)
+        {
+            MyBase::operator=(rhs);
+            return *this;
+        }
+
+        set & operator=(set && rhs)
+        {
+            MyBase::operator=(std::move(rhs));
+            return *this;
+        }
+    };
+
+
+    template<typename TKey, typename TCompare, typename Allocator>
+    struct MultiSet_TreeTraits
+    {
+        using key_type       = TKey;
+        using value_type     = TKey;
+        using key_compare    = TCompare;
+        using value_compare  = TCompare;
+        using allocator_type = Allocator;
+        using IsMulti        = details::TrueType;
+
+        static const key_type & getKey(const value_type & value) noexcept
+        {
+            return value;
+        }
+    };
+
+
+    template<typename TKey,
+        typename Compare = std::less<TKey>,
+        typename Allocator = std::allocator<TKey >>
+    class multiset : public details::Tree<MultiSet_TreeTraits<TKey, Compare, Allocator>>
+    {
+    private:
+        using MyBase = details::Tree<MultiSet_TreeTraits<TKey, Compare, Allocator>>;
+
+    public:
+        using MyBase::Tree;
+
+        multiset() :
+            multiset(key_compare()) { }
+
+        multiset(const multiset & rhs) :
+            MyBase(rhs) { }
+
+        multiset(multiset && rhs) :
+            MyBase(std::move(rhs)) { }
+
+        multiset & operator=(const multiset & rhs)
+        {
+            MyBase::operator=(rhs);
+            return *this;
+        }
+
+        multiset & operator=(multiset && rhs)
+        {
+            MyBase::operator=(std::move(rhs));
+            return *this;
+        }
     };
 }

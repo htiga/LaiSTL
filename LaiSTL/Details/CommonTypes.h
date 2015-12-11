5867f7f2c8cb7847d02040f553d4481e84b16d7b
htiga <htiga@hotmail.com>
2015-12-11 11:31:25 +0800

    Add typedefs

--- LaiSTL/Details/CommonTypes.h
+++ LaiSTL/Details/CommonTypes.h
@@ -1,5 +1,5 @@
 #pragma once
-
+// Types for argument dispatch
 
 namespace lai
 {
@@ -19,5 +19,23 @@ namespace lai
         {
             // empty struct for dispatch
         };
+
+        template<bool, typename T1, typename T2>
+        struct Select
+        {
+            // false, select T2
+            using type = T2;
+        };
+
+        template<typename T1, typename T2>
+        struct Select<true, T1, T2>
+        {
+            // true, select T1
+            using type = T1;
+        };
+
+        // Select T1 if pred else T2
+        template<bool pred, typename T1, typename T2>
+        using SelectType = typename Select<pred, T1, T2>::type;
     }
 }

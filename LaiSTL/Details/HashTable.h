67d4fcf8fe906c2a219a41e5afc8de07566a879b
htiga <htiga@hotmail.com>
2015-12-11 11:35:25 +0800

    ...

--- LaiSTL/Details/HashTable.h
+++ LaiSTL/Details/HashTable.h
@@ -1,1 +1,21 @@
 #pragma once
+
+namespace lai
+{
+    namespace details
+    {
+        // HashTraits must contain following public members :
+        //      typedefs : key_type
+        //                 value_type 
+        //                 hasher
+        //                 key_equal
+        //                 allocator_type
+        //                 IsMulti (std::true_type or std::false_type)
+        //      static const key_type & getKey(const value_type & value);
+        template<typename HashTraits>
+        class HashTable
+        {
+
+        };
+    }
+}

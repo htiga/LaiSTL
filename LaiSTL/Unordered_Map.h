3b4bed50c38fada9cf02aca7e50e4d3d42ab96b9
htiga <htiga@hotmail.com>
2015-12-21 00:27:32 +0800

    Add unoredered_multimap

--- LaiSTL/Unordered_Map.h
+++ LaiSTL/Unordered_Map.h
@@ -41,4 +41,40 @@ namespace lai
         using MyBase::HashTable;
     };
 
+
+    template<typename TKey,
+        typename TValue,
+        typename Hash,
+        typename KeyEqual,
+        typename Allocator>
+    struct UMultiMap_HashTraits
+    {
+        using key_type       = TKey;
+        using value_type     = std::pair<const TKey, TValue>;
+        using hasher         = Hash;
+        using key_equal      = KeyEqual;
+        using allocator_type = Allocator;
+        using IsMulti        = details::TrueType;
+
+        static const key_type & getKey(const value_type & value) noexcept
+        {
+            return value.first;
+        }
+    };
+
+
+    template<typename TKey,
+        typename TValue,
+        typename Hash = std::hash<TKey>,
+        typename KeyEqual = std::equal_to<TKey>,
+        typename Allocator = std::allocator<std::pair<const TKey, TValue>>>
+    class unordered_multimap :
+        public details::HashTable<UMultiMap_HashTraits<TKey, TValue, Hash, KeyEqual, Allocator>>
+    {
+    private:
+        using MyBase = details::HashTable<
+            UMultiMap_HashTraits<TKey, TValue, Hash, KeyEqual, Allocator>>;
+    public:
+        using MyBase::HashTable;
+    };
 }

5e177e93a41a1fcdb4bbe35d678104fac3e209cb
htiga <htiga@hotmail.com>
2015-12-21 00:24:42 +0800

    Add unordered_map

--- LaiSTL/Unordered_Map.h
+++ LaiSTL/Unordered_Map.h
@@ -1,1 +1,44 @@
 #pragma once
+#include "Details\HashTable.h"
+#include <functional>
+#include <memory>
+#include <utility>
+
+namespace lai
+{
+    template<typename TKey,
+        typename TValue,
+        typename Hash,
+        typename KeyEqual,
+        typename Allocator>
+    struct UMap_HashTraits
+    {
+        using key_type       = TKey;
+        using value_type     = std::pair<const TKey, TValue>;
+        using hasher         = Hash;
+        using key_equal      = KeyEqual;
+        using allocator_type = Allocator;
+        using IsMulti        = details::FalseType;
+
+        static const key_type & getKey(const value_type & value) noexcept
+        {
+            return value.first;
+        }
+    };
+
+
+    template<typename TKey,
+        typename TValue,
+        typename Hash = std::hash<TKey>,
+        typename KeyEqual = std::equal_to<TKey>,
+        typename Allocator = std::allocator<std::pair<const TKey, TValue>>>
+    class unordered_map :
+        public details::HashTable<UMap_HashTraits<TKey, TValue, Hash, KeyEqual, Allocator>>
+    {
+    private:
+        using MyBase = details::HashTable<UMap_HashTraits<TKey, TValue, Hash, KeyEqual, Allocator>>;
+    public:
+        using MyBase::HashTable;
+    };
+
+}

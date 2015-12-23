b28def615fb9dc20ff2029fbb3d446027f46fbf1
htiga <htiga@hotmail.com>
2015-12-23 19:31:44 +0800

    Add tests

---
+++ UnitTest/TestCases/TestUnorderedMultiSet.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "../LaiSTL/Unordered_Set.h"
+#include <set>
+#include <functional>
+
+#define LAI_IUMSET lai::unordered_multiset<int>
+#define LAI_SUMSET lai::unordered_multiset<std::string>
+#define LAI_UUMSET lai::unordered_multiset<Uncopyable>
+
+namespace UnitTest
+{
+
+}
